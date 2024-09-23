47aac665cf75de77170ec43173cf626cbc569b1d
ly <zengxianzhao.zxz@antgroup.com>
2024-09-23 16:40:15 +0800

    first commit

---
+++ src/main/java/org/changsha/changshapoc/ChangshaPocApplication.java
@@ -0,0 +1,13 @@
+package org.changsha.changshapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangshaPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChangshaPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/changsha/changshapoc/ChangshaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package org.changsha.changshapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangshaPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
